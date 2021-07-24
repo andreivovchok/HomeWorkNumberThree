c765806b478853c3f0b8b38257a51310b3882d5c
Andrei Vovchok <andreivovchok@bk.ru>
2021-07-24 19:55:36 +0300

    Добавил решение

---
+++ HomeWorkNumberThree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWorkNumberThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double i;
+            const double PI = 3.141;
+
+            Console.WriteLine("Введите значение радиуса основания");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение образующей");
+            i = Convert.ToDouble(Console.ReadLine());
+
+            double S = PI * r * (r + i);
+            Console.WriteLine(S);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWorkNumberThree3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkNumberThree3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
