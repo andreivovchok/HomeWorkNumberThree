03c177cc2beab3d5b846a0a22932748b458a56a1
Andrei Vovchok <andreivovchok@bk.ru>
2021-07-25 14:25:26 +0300

    добавил решение

--- HomeWorkNumberThree2/Program.cs
+++ HomeWorkNumberThree2/Program.cs
@@ -7,46 +7,17 @@ namespace HomeWorkNumberThree2
         static void Main(string[] args)
         {
             int x = 14, y = 1, z = 5;
+            x += y - x++ * z;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            z = --x - y * 5;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            y /= x + 5 % z;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            z = x++ + y * 5;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            x = y - x++ * z;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
 
-            x = x + y - x++ * z;
-            var a = 14 + 1 - 14 * 5;
-            x++;
-            x = a;
-            x = Minus(Plus(x, y), Umnozh(x++, z));
-
-            int b = z = --x - y * 5;
-            int c = y /= x + 5 % z;
-            int d = z = x++ + y * 5;
-            int e = x = y - x++ * z;
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-
-            Console.ReadKey();
-
-        }
-
-        static int Plus(int first, int second)
-        {
-            return first + second;
-        }
-
-        static int Minus(int first, int second)
-        {
-            return first - second;
-        }
-
-        static int Umnozh(int first, int second)
-        {
-            return first * second;
-        }
-
-        static int Delit(int first, int second)
-        {
-            return first / second;
         }
     }
 }
